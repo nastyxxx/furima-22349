1c926937b6a8a6c45737b057e8b5d5bdb5128ac4
nastyxxx <naotsu0418@gmail.com>
2020-10-22 12:40:12 +0900

    モデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,19 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :email,           presence: true
   validates :password,
-    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'Include both letters and numbers' },
-    presence: true
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze, message: 'Include both letters and numbers' }
   validates :first_name,
-    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' },
-    presence: true
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' },
+            presence: true
   validates :last_name,
-    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' },
-    presence: true
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' },
+            presence: true
   validates :first_name_kana,
-    format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' },
-    presence: true
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' },
+            presence: true
   validates :last_name_kana,
-    format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' },
-    presence: true
-  validates :birth_day,       presence: true
-  
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' },
+            presence: true
+  validates :birth_day, presence: true
 end
